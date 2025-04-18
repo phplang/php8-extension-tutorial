fb0f80ef13fc009f2bf1f6ff127598a1a0307923
Sara Golemon <pollita@php.net>
2025-04-18 13:51:23 +0000

    mygmp_random_ints()

--- mygmp.stub.php
+++ mygmp.stub.php
@@ -7,3 +7,5 @@ function mygmp_version(): void {}
 function mygmp_get_version(): string {}
 
 function mygmp_add(string $a, string $b): string|false {}
+
+function mygmp_random_ints(int $count, int $bits = 64): array {}
